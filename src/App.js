aff1930cd24eff41662581f42b80d63bf8103c05
Beiny <apatenko.nikita31rus@yandex.ru>
2023-05-23 23:41:24 +0300

    Добавлен редукс

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import { Provider } from 'react-redux';
 import {useParams} from 'react-router-dom';
 import { HomePage } from './components/home-page/home-page';
 import { Header } from './components/header/header';
+import { store } from './redux';
 
 
 export default function App() {
   return (
-    <Router>
-      <div className='App'>
-      <Header/>
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Header</Link>
-            </li>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-        </nav> */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
+    <Provider store={store}>
+        <Router>
+          <div className='App'>
+            <Header/>
+                {/* <nav>
+                  <ul>
+                    <li>
+                      <Link to="/">Header</Link>
+                    </li>
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                  </ul>
+                </nav> */}
+              <Routes>
+                  <Route path="/" element={<HomePage />} />
+              </Routes>
+          </div>
+      </Router>
+    </Provider>
   );
 }
--- src/components/gameBuy/gameBuy.jsx
+++ src/components/gameBuy/gameBuy.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import './gameBuy.css'
 import { Button } from '../button';
+import { setItemInCart } from '../../redux/cart/reducer';
 
 export const GameBuy = ({ game }) => {
+    const dispatch = useDispatch();
+
+    const handleClick = (e) => {
+        e.stopPropagation();
+        dispatch(setItemInCart(game));
+    }
+
     return (
         <div className="game-buy">
           <span className='game-buy__price'>{game.price} руб.</span>
           <Button 
             type='primary'
-            onClick={ () => null }
+            onClick={ handleClick }
           >
                 В Корзину
           </Button>
